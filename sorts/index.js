6d4148fad4ed0d33bfbc43707331e21c0d7b5997
Peter Shafer <peter.shafer@gmail.com>
2017-05-21 13:47:44 -0700

    Let's add some more comments and context for each of the provided sorts.

--- sorts/index.js
+++ sorts/index.js
@@ -19,11 +19,13 @@ class xArray {
       next.push(temp);
     }
     this.log(next);
-    this.log("-=-=-=-=-=-=-=");
     this.data = next;
   }
 
   selectionSort() {
+    // Selection sort sorts an array by partitioning it and "selecting"
+    // the next value by finding the maximum in the remaining unsorted
+    // values, until none remain. 
     const arr = this.data;
     this.log(arr);
     for(let i = 0; i < arr.length; i++){
@@ -43,6 +45,11 @@ class xArray {
   }
 
   bubbleSort() {
+    // Bubble sort will "bubble" up the maximum value on each subsequent pass.
+    // The maximum value isn't explicitly sought each time, but it creates
+    // a partition on the latter half of the array of sorted values in
+    // ascending order. It's not implemented as the inverse of selection sort
+    // but that's what we find happening with each pass.
     const arr = this.data;
     for(let i = 0; i < arr.length - 1; i++){
       // Assume with each iteration that max value has bubbled to top.
@@ -63,12 +70,25 @@ class xArray {
   }
 
   insertionSort() {
+    // Insertion sort is similar to selection sort, but instead of finding
+    // the exact value you want to add to the partition of sorted values,
+    // you just take the next unsorted value and then move it through the
+    // sorted values in the partition until you find where it should be
+    // inserted.
     const arr = this.data;
+    // We can assume the first item has already been inserts, so start
+    // at i = 1 to begin.
     for(let i = 1; i < arr.length; i++){
+      // Next, we need to iterate through the sorted values from greatest
+      // to least. So we set up the next loop to start at the item just
+      // before the value to be inserted.
       let j = i-1;
       while(arr[i] < arr[j] && j >= 0){
         j--;
       }
+      // We know where to insert the item, insert it into the index 
+      // immediately following the first item (found in reverse order) 
+      // in the sorted list that the inserted item is greater than.
       let temp = arr.splice(i,1).pop();
       arr.splice(j+1,0,temp);
       this.log(arr);
