7ad7c11cf6ca0d342425621209f792251ef87a19
Peter Shafer <peter.shafer@gmail.com>
2017-05-25 16:23:19 -0700

    Write some more tests for sorting algorithm implementations

--- sorts/index.test.js
+++ sorts/index.test.js
@@ -11,6 +11,22 @@ var buildRandomArray = (size=1000) => {
   return arr;
 }
 
+describe(`xArray constructor`, function() {
+  it('should initialize an array if argument specified', function() {
+    let values = buildRandomArray(10);
+    let arr = new xArray(values);
+    expect(arr.data).to.be.an('array');
+    for(let i = 0; i < values.length; i++){
+      expect(arr.data[i]).to.be.equal(values[i]);
+    }
+  });
+  it('should initialize an empty array if no argument specified', function() {
+    let arr = new xArray();
+    expect(arr.data).to.be.an('array');
+    expect(arr.data.length).to.be.equal(0);
+  });
+});
+
 var algorithms = ["nativeSort", "heapSort", "radixSort", "mergeSort", "quickSort", "bubbleSort", "insertionSort", "selectionSort", "countingSort"];
 
 algorithms.forEach((algorithm) => {
@@ -26,5 +42,3 @@ algorithms.forEach((algorithm) => {
   });
 
 });
-
-
